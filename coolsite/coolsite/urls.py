ded7fd6f4a71c9144ff62bd25e8cb6272e250512
NeMmiddle <SteamEGTS322@yandex.ru>
2022-09-30 20:30:34 +0300

    Again Django

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -25,8 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('women.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
