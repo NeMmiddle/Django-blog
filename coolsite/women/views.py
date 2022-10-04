209a954cf99673a4120d4f73f7cfb8e219eab273
NeMmiddle <SteamEGTS322@yandex.ru>
2022-10-04 20:24:18 +0300

    Registration

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,4 +1,7 @@
+
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
@@ -11,6 +14,7 @@ from .utils import *
 
 
 class WomenHome(DataMixin, ListView):
+    paginate_by = 3
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'posts'
@@ -37,7 +41,12 @@ class WomenHome(DataMixin, ListView):
 #     return render(request, 'women/index.html', context=context)
 
 def about(request):
-    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+    contact_list = Women.objects.all()
+    paginator = Paginator(contact_list, 3)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'women/about.html', {'page_obj': page_obj, 'menu': menu, 'title': 'О сайте'})
 
 
 class AddPage(LoginRequiredMixin, DataMixin, CreateView):
@@ -97,7 +106,8 @@ class ShowPost(DataMixin, DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        return context
+        c_def = self.get_user_context(title=context['post'])
+        return {**context, **c_def}
 
 
 class WomenCategory(DataMixin, ListView):
@@ -129,3 +139,19 @@ class WomenCategory(DataMixin, ListView):
 #     }
 #
 #     return render(request, 'women/index.html', context=context)
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return {**context, **c_def}
+
+
+
+
+
