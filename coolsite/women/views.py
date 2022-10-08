95e1485de7a66faf09a10e1f4a293582407897da
NeMmiddle <SteamEGTS322@yandex.ru>
2022-10-08 13:14:02 +0300

    Login + logout

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -159,16 +159,18 @@ class RegisterUser(DataMixin, CreateView):
 
 
 class LoginUser(DataMixin, LoginView):
-    form_class = AuthenticationForm
+    form_class = LoginUserForm
     template_name = 'women/login.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Авторизация")
-        return dict(list(context.items()) + list(c_def.items()))
+        return {**context, **c_def}
 
     def get_success_url(self):
         return reverse_lazy('home')
 
 
-
+def logout_user(request):
+    logout(request)
+    return redirect('login')
